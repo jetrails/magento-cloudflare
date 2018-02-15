0e807b07acda07a7f3de4888df3b481b0a18b1c2
null93 <rdogg312@gmail.com>
2018-02-14 23:55:59 -0600

    Added Api Helper class

---
+++ src/app/code/community/JetRails/Cloudflare/Helper/Api.php
@@ -0,0 +1,21 @@
+<?php
+
+	class JetRails_Cloudflare_Helper_Api extends Mage_Core_Helper_Abstract {
+
+		private $authEmail;
+		private $authKey;
+
+		public function __construct () {
+			$this->$authEmail = "";
+			$this->$authKey = "";
+		}
+
+		private function _makeRequest ( $type, $payload ) {
+
+		}
+
+		public function get (  ) {
+
+		}
+
+	}
