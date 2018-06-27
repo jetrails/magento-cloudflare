7ca39b039b32975e5bbde4ef883a40417317def6
Rafael Grigorian <me@raffi.io>
2018-06-27 12:21:22 -0500

    Fixed GH-76

--- src/skin/adminhtml/base/default/js/cloudflare/crypto/require_modern_tls.js
+++
@@ -1,5 +0,0 @@
-const $ = require ("jquery")
-const switchElement = require ("cloudflare/generic/switch")
-
-$(document).on ( "cloudflare.crypto.require_modern_tls.initialize", switchElement.initialize )
-$(document).on ( "cloudflare.crypto.require_modern_tls.toggle", switchElement.toggle )
