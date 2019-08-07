ef6dec1070cc0128e98fa26777ab223f90269ebf
Rafael Grigorian <me@raffi.io>
2019-08-07 11:11:22 -0500

    Fixed #91

---
+++ src/app/code/community/JetRails/Cloudflare/Model/Adminhtml/Api/Network/TrueClientIpHeader.php
@@ -0,0 +1,24 @@
+<?php
+
+	/**
+	 * This model class inherits from the Setter model.  It essentially wraps
+	 * that class in order to send passed data to the Cloudflare API endpoint.
+	 * @version     1.1.0
+	 * @package     JetRails® Cloudflare
+	 * @author      Rafael Grigorian <development@jetrails.com>
+	 * @copyright   © 2018 JETRAILS, All rights reserved
+	 * @license     MIT https://opensource.org/licenses/MIT
+	 */
+	class JetRails_Cloudflare_Model_Adminhtml_Api_Network_TrueClientIpHeader
+	extends JetRails_Cloudflare_Model_Adminhtml_Api_Setter {
+
+		/**
+		 * @var     string      _endpoint       Appended to zone endpoint
+		 * @var     string      _dataKey        Key name used for value
+		 * @var     integer     _settingType    Value cast type before sending
+		 */
+		protected $_endpoint = "settings/true_client_ip_header";
+		protected $_dataKey = "value";
+		protected $_settingType = self::TYPE_SWITCH;
+
+	}
