406bd9338ca4c28d036b43b5d9ee2ec11fbf12a6
Rafael Grigorian <me@raffi.io>
2019-08-14 10:30:58 -0500

    Released Version 1.1.1 (#97)
    Fixed #96

--- src/app/code/community/JetRails/Cloudflare/controllers/Cloudflare/DashboardController.php
+++ src/app/code/community/JetRails/Cloudflare/controllers/Cloudflare/DashboardController.php
@@ -6,7 +6,7 @@
 	 * zone and token that is used for API access. Finally it contains an
 	 * action to change the domain to use with the Cloudflare dashboard. This
 	 * domain selection is saved within the user's session.
-	 * @version     1.1.0
+	 * @version     1.1.1
 	 * @package     JetRails® Cloudflare
 	 * @author      Rafael Grigorian <development@jetrails.com>
 	 * @copyright   © 2018 JETRAILS, All rights reserved
