26266f6443b6d93f8c82e0330bf735eef4cdba73
Rafael Grigorian <me@raffi.io>
2019-09-16 12:51:59 -0500

    Fixed #99

--- src/app/code/community/JetRails/Cloudflare/Model/Adminhtml/Api/Overview/Configuration.php
+++ src/app/code/community/JetRails/Cloudflare/Model/Adminhtml/Api/Overview/Configuration.php
@@ -13,43 +13,6 @@
 	extends Mage_Core_Model_Abstract {
 
 		/**
-		 * This method caches a zone value based on a passed domain. This is
-		 * used cache the result of an API call within an entire session. The
-		 * zone id is cached, because it gets used a lot throughout a typical
-		 * session.
-		 * @param   string       domain               Domain to cache as key
-		 * @param   string       zone                 The zone to cache as value
-		 * @return  void
-		 */
-		protected function _setCached ( $domain, $zone ) {
-			$session = Mage::getSingleton ("core/session");
-			$lookup = array ();
-			if ( !empty ( $session->getDomainZone () ) ) {
-				$lookup = $session->getDomainZone ();
-			}
-			$lookup [ "$domain" ] = "$zone";
-			$session->setDomainZone ( $lookup );
-		}
-
-		/**
-		 * This method attempts to retrieve a zone id from cache, based on a
-		 * domain name. If there is no zone id for the passed domain name, then
-		 * false is returned.
-		 * @param   string       domain               Domain for zone id
-		 * @return  mixed                             Cached zone id or false
-		 */
-		protected function _getCached ( $domain ) {
-			$session = Mage::getSingleton ("core/session");
-			$lookup = $session->getDomainZone ();
-			if ( !empty ( $lookup )
-				 && is_array ( $lookup )
-				 && array_key_exists ( "$domain", $lookup ) ) {
-				return $lookup ["$domain"];
-			}
-			return false;
-		}
-
-		/**
 		 * This method takes in a zone and a token. It then makes an API call to
 		 * Cloudflare and finds out if the supplied token for the zone is valid.
 		 * @param   string       zone                CF authentication zone
@@ -60,6 +23,11 @@
 			$api = Mage::getModel ("cloudflare/api_request");
 			$api->setType ( $api::REQUEST_GET );
 			$response = $api->resolve ("user/tokens/verify");
+			if ( !$response->success ) {
+				return false;
+			}
+			if ( !$zone ) $zone = $this->getZoneId ();
+			$response = $api->resolve ("zones/$zone");
 			return $response->success;
 		}
 
@@ -71,14 +39,7 @@
 		 */
 		public function getZoneId () {
 			$data = Mage::helper ("cloudflare/data");
-			$domain = $data->getDomainName ();
-			$cached = $this->_getCached ("$domain");
-			if ( $cached !== false ) {
-				return $cached;
-			}
-			$zone = $data->getAuthZone ();
-			$this->_setCached ( "$domain", $zone );
-			return $zone;
+			return $data->getAuthZone ();
 		}
 
 	}
