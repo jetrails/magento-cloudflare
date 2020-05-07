965db251271553e56424701d39498169ec62efb3
Rafael Grigorian <me@raffi.io>
2020-05-07 13:48:41 -0500

    Released Version 1.1.4

--- src/app/code/community/JetRails/Cloudflare/Model/Adminhtml/Api/Overview/Configuration.php
+++ src/app/code/community/JetRails/Cloudflare/Model/Adminhtml/Api/Overview/Configuration.php
@@ -3,7 +3,7 @@
 	/**
 	 * This class handles the logic to authenticate a zone/token pair through
 	 * the use of the Cloudflare API.
-	 * @version     1.1.3
+	 * @version     1.1.4
 	 * @package     JetRails® Cloudflare
 	 * @author      Rafael Grigorian <development@jetrails.com>
 	 * @copyright   © 2018 JETRAILS, All rights reserved
