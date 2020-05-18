4186dc297491544cf21d8587b6bbe9ebbf1662c5
Rafael Grigorian <me@raffi.io>
2020-05-18 11:13:28 -0500

    Fixed #107

---
+++ src/app/code/community/JetRails/Cloudflare/Model/Adminhtml/Api/Network/ZeroRttConnectionResumption.php
@@ -0,0 +1,24 @@
+<?php
+
+	/**
+	 * This model class inherits from the Setter model.  It essentially wraps
+	 * that class in order to send passed data to the Cloudflare API endpoint.
+	 * @version     1.1.5
+	 * @package     JetRails® Cloudflare
+	 * @author      Rafael Grigorian <development@jetrails.com>
+	 * @copyright   © 2018 JETRAILS, All rights reserved
+	 * @license     MIT https://opensource.org/licenses/MIT
+	 */
+	class JetRails_Cloudflare_Model_Adminhtml_Api_Network_ZeroRttConnectionResumption
+	extends JetRails_Cloudflare_Model_Adminhtml_Api_Setter {
+
+		/**
+		 * @var     string      _endpoint       Appended to zone endpoint
+		 * @var     string      _dataKey        Key name used for value
+		 * @var     integer     _settingType    Value cast type before sending
+		 */
+		protected $_endpoint = "settings/0rtt";
+		protected $_dataKey = "value";
+		protected $_settingType = self::TYPE_SWITCH;
+
+	}
