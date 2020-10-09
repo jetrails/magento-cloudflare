ddb266cacafae0f57ff5d787a7c13282903bf9e6
Rafael Grigorian <me@raffi.io>
2020-10-09 10:40:32 -0500

    Fixed #128

---
+++ src/app/code/community/JetRails/Cloudflare/Model/Adminhtml/Api/Speed/MobileRedirect.php
@@ -0,0 +1,71 @@
+<?php
+
+	/**
+	 * This model inherits from the basic Getter model. It inherits
+	 * functionality that asks the Cloudflare API for a current setting value.
+	 * It then adds on to that functionality by adding more methods that
+	 * interact with the Cloudflare API.
+	 * @version     1.1.7
+	 * @package     JetRails® Cloudflare
+	 * @author      Rafael Grigorian <development@jetrails.com>
+	 * @copyright   © 2018 JETRAILS, All rights reserved
+	 * @license     MIT https://opensource.org/licenses/MIT
+	 */
+	class JetRails_Cloudflare_Model_Adminhtml_Api_Speed_MobileRedirect
+	extends JetRails_Cloudflare_Model_Adminhtml_Api_Getter {
+
+		/**
+		 * @var     string      _endpoint             Appended to zone endpoint
+		 */
+		protected $_endpoint = "settings/mobile_redirect";
+
+		/**
+		 * This method contacts the Cloudflare API and asks for the current
+		 * value for the setting but also returns domain name options.
+		 * @return  stdClass                          CF response to request
+		 */
+		public function getValue () {
+			$apiMobileRedirect = Mage::getModel ("cloudflare/api_request");
+			$apiMobileRedirect->setType ( $apiMobileRedirect::REQUEST_GET );
+			$setting = $apiMobileRedirect->resolve ( $this->getEndpoint () );
+			if ( isset ( $setting->success ) && $setting->success ) {
+				$apiDomains = Mage::getModel ("cloudflare/api_request");
+				$apiDomains->setType ( $apiDomains::REQUEST_GET );
+				$apiDomains->setQuery ( "per_page", 1000 );
+				$apiDomains->setQuery ( "name", "starts_with:" );
+				$apiDomains->setQuery ( "type", "A,AAAA,CNAME" );
+				$domains = $apiDomains->resolve ( $this->getEndpoint ("dns_records") );
+				$setting->result->domains = array_map ( function ( $domain ) {
+					preg_match ( "/^(.*)\..*?\..*$/m", $domain->name, $match );
+					return array (
+						"value" => $match [ 1 ],
+						"label" => $domain->name . " (" . $domain->type . ")",
+					);
+				}, $domains->result );
+			}
+			return $setting;
+		}
+
+		/**
+		 * This method takes in the options as arguments and transforms that
+		 * into a valid request.
+		 * @param   string      mobileSubdomain       Subdomain to use
+		 * @param   string      status                Expecting 'on' or 'off'
+		 * @param   boolean     stripUri              Drop path or keep it?
+		 * @return  stdClass                          CF response to request
+		 */
+		public function change ( $mobileSubdomain, $status, $stripUri ) {
+			$data = array (
+				"mobile_subdomain" => $mobileSubdomain,
+				"status" => $status,
+				"strip_uri" => $stripUri
+			);
+			$endpoint = $this->getEndpoint ();
+			$api = Mage::getModel ("cloudflare/api_request");
+			$api->setType ( $api::REQUEST_PATCH );
+			$api->setData ( array ( "value" => $data ) );
+			$response = $api->resolve ( $endpoint );
+			return $response;
+		}
+
+	}
---
+++ src/app/code/community/JetRails/Cloudflare/controllers/Cloudflare/Api/Speed/MobileRedirectController.php
@@ -0,0 +1,45 @@
+<?php
+
+	/**
+	 * This controller inherits from the base action class and it inherits
+	 * helper methods that are contained within it. This controller contains two
+	 * actions. One gets the values for the polish and webp settings and the
+	 * other changes them.
+	 * @version     1.1.7
+	 * @package     JetRails® Cloudflare
+	 * @author      Rafael Grigorian <development@jetrails.com>
+	 * @copyright   © 2018 JETRAILS, All rights reserved
+	 * @license     MIT https://opensource.org/licenses/MIT
+	 */
+	class JetRails_Cloudflare_Cloudflare_Api_Speed_MobileRedirectController
+	extends JetRails_Cloudflare_Controller_Action {
+
+		/**
+		 * This action asks the Cloudflare API model for the value of the mobile
+		 * redirect setting and the available DNS entries. It then combines both
+		 * responses and sends it back to the caller.
+		 * @return  void
+		 */
+		public function indexAction () {
+			$api = Mage::getModel ("cloudflare/api_speed_mobileRedirect");
+			$response = $api->getValue ();
+			return $this->_sendResponse ( $response );
+		}
+
+		/**
+		 * This action takes in a polish value and a webp value through the
+		 * request parameters. It then uses the Cloudflare API model to change
+		 * the passed settings.
+		 * @return  void
+		 */
+		public function changeAction () {
+			$api = Mage::getModel ("cloudflare/api_speed_mobileRedirect");
+			$response = $api->change (
+				$this->_request->getParam ("mobile_subdomain"),
+				$this->_request->getParam ("status") === "on" ? "on" : "off",
+				$this->_request->getParam ("strip_uri") === "true"
+			);
+			return $this->_sendResponse ( $response );
+		}
+
+	}
