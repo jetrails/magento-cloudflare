157bdae42695ee89535b431c8efc66569e8a2a01
Rafael Grigorian <me@raffi.io>
2020-10-12 14:05:01 -0500

    Fixed #136

---
+++ src/app/code/community/JetRails/Cloudflare/Model/Adminhtml/Api/Dns/CustomNameservers.php
@@ -0,0 +1,60 @@
+<?php
+
+	/**
+	 * This model inherits from the basic Getter model. It inherits
+	 * functionality that asks the Cloudflare API for a current setting value.
+	 * It then adds on to that functionality by adding more methods that
+	 * interact with the Cloudflare API.
+	 * @version     1.1.7
+	 * @package     JetRails® Cloudflare
+	 * @author      Rafael Grigorian <development@jetrails.com>
+	 * @copyright   © 2018 JETRAILS, All rights reserved
+	 * @license     MIT https://opensource.org/licenses/MIT
+	 */
+	class JetRails_Cloudflare_Model_Adminhtml_Api_Dns_CustomNameservers
+	extends JetRails_Cloudflare_Model_Adminhtml_Api_Getter {
+
+		/**
+		 * @var     string      _endpoint             Appended to zone endpoint
+		 */
+		protected $_endpoint = "";
+
+		/**
+		 * This method simply constructs a GET request to the endpoint that is
+		 * most appropriate based on how this object was configured.
+		 * @return  stdClass                          CF response to request
+		 */
+		public function getValue () {
+			$endpoint = $this->getEndpoint ();
+			$api = Mage::getModel ("cloudflare/api_request");
+			$api->setType ( $api::REQUEST_GET );
+			$response = $api->resolve ( $endpoint );
+			if ( $response->success ) {
+				$response->result = $response->result->vanity_name_servers_ips
+					? $response->result->vanity_name_servers_ips
+					: new stdClass ();
+			}
+			return $response;
+		}
+
+		/**
+		 * This method takes in a list of custom nameservers and it updates it
+		 * accordingly using Cloudflare's API.
+		 * @param   array        records              List of custom nameservers
+		 * @return  stdClass                          CF response to request
+		 */
+		public function edit ( $records ) {
+			$endpoint = $this->getEndpoint ();
+			$api = Mage::getModel ("cloudflare/api_request");
+			$api->setType ( $api::REQUEST_PATCH );
+			$api->setData ( array ( "vanity_name_servers" => $records ));
+			$response = $api->resolve ( $endpoint );
+			if ( $response->success ) {
+				$response->result = $response->result->vanity_name_servers_ips
+					? $response->result->vanity_name_servers_ips
+					: new stdClass ();
+			}
+			return $response;
+		}
+
+	}
---
+++ src/app/code/community/JetRails/Cloudflare/controllers/Cloudflare/Api/Dns/CustomNameserversController.php
@@ -0,0 +1,32 @@
+<?php
+
+	/**
+	 * This controller inherits from a generic controller that implements the
+	 * base functionality for interfacing with a getter model. This action
+	 * simply loads the initial value through the Cloudflare API. The rest of
+	 * this class extends on that functionality and adds more endpoints.
+	 * @version     1.1.7
+	 * @package     JetRails® Cloudflare
+	 * @author      Rafael Grigorian <development@jetrails.com>
+	 * @copyright   © 2018 JETRAILS, All rights reserved
+	 * @license     MIT https://opensource.org/licenses/MIT
+	 */
+	class JetRails_Cloudflare_Cloudflare_Api_Dns_CustomNameserversController
+	extends JetRails_Cloudflare_Controller_Getter {
+
+		/**
+		 * This action takes in custom nameserver records and it then asks the
+		 * Cloudflare API model to update the values for the zone.
+		 * @return  void
+		 */
+		public function editAction () {
+			$api = Mage::getModel ("cloudflare/api_dns_customNameservers");
+			$response = $api->edit (
+				$this->_request->getParam ("nameservers")
+				? $this->_request->getParam ("nameservers")
+				: array ()
+			);
+			return $this->_sendResponse ( $response );
+		}
+
+	}
