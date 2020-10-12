2f1c6f7acaeab6136bcb49941e2d23bb1a5d4a31
Rafael Grigorian <me@raffi.io>
2020-10-12 12:12:15 -0500

    Fixed #135

---
+++ src/app/code/community/JetRails/Cloudflare/Model/Adminhtml/Api/Firewall/ZoneLockdown.php
@@ -0,0 +1,115 @@
+<?php
+
+	/**
+	 * This model inherits from the basic Getter model. It inherits
+	 * functionality that asks the Cloudflare API for a current setting value.
+	 * It then adds on to that functionality by adding more methods that
+	 * interact with the Cloudflare API.
+	 * @version     1.1.7
+	 * @package     JetRails® Cloudflare
+	 * @author      Rafael Grigorian <development@jetrails.com>
+	 * @copyright   © 2018 JETRAILS, All rights reserved
+	 * @license     MIT https://opensource.org/licenses/MIT
+	 */
+	class JetRails_Cloudflare_Model_Adminhtml_Api_Firewall_ZoneLockdown
+	extends JetRails_Cloudflare_Model_Adminhtml_Api_Getter {
+
+		/**
+		 * @var     string      _endpoint             Appended to zone endpoint
+		 * @var     boolean     _usePatchToSet        Use PUT HTTP method
+		 */
+		protected $_endpoint = "firewall/lockdowns";
+		protected $_usePatchToSet = false;
+
+		/**
+		 * This method wraps the parent method because we want to get the value
+		 * that the parent returns and then append the entitlements result to
+		 * it.
+		 * @return  stdClass                          CF response to request
+		 */
+		public function getValue () {
+			$result = (array) parent::getValue ();
+			$result ["entitlements"] = $this->getEntitlements ();
+			return (object) $result;
+		}
+
+		/**
+		 * This method simply contacts the Cloudflare API and asks for a list of
+		 * entitlements. It then only returns the entitlements that are related
+		 * to zone lockdown rules.
+		 * @return  stdClass                          CF response to request
+		 */
+		public function getEntitlements () {
+			$endpoint = $this->getEndpoint ("entitlements");
+			$api = Mage::getModel ("cloudflare/api_request");
+			$api->setType ( $api::REQUEST_GET );
+			$response = $api->resolve ( $endpoint );
+			return current ( array_filter ( $response->result, function ( $i ) {
+				return $i->id === "zonelockdown.max_rules";
+			}));
+		}
+
+		/**
+		 * This method takes in information about a zone lockdown and creates it
+		 * using Cloudflare's API.
+		 * @param   string       desc                 Zone lockdown description
+		 * @param   array        urls                 Array of URLs to use
+		 * @param   array        config               Mixtrure of IPs and IP Ranges
+		 * @param   boolean      paused               Is it paused?
+		 * @param   integer      priority             What is the priority, default: none
+		 * @return  stdClass                          CF response to request
+		 */
+		public function create ( $desc, $urls, $config, $paused, $priority = null ) {
+			$endpoint = $this->getEndpoint ();
+			$api = Mage::getModel ("cloudflare/api_request");
+			$api->setType ( $api::REQUEST_POST );
+			$api->setData ( array (
+				"description" => $desc,
+				"urls" => $urls,
+				"configurations" => $config,
+				"paused" => $paused,
+				"priority" => $priority
+			));
+			return $api->resolve ( $endpoint );
+		}
+
+		/**
+		 * This method takes in information about an already created zone
+		 * lockdown and it updates it accordingly.
+		 * using Cloudflare's API.
+		 * @param   string       id                   Page rule ID
+		 * @param   string       desc                 Zone lockdown description
+		 * @param   array        urls                 Array of URLs to use
+		 * @param   array        config               Mixtrure of IPs and IP Ranges
+		 * @param   boolean      paused               Is it paused?
+		 * @param   integer      priority             What is the priority, default: none
+		 * @return  stdClass                          CF response to request
+		 */
+		public function edit ( $id, $desc, $urls, $config, $paused, $priority = null ) {
+			$endpoint = $this->getEndpoint ( $this->_endpoint . "/$id" );
+			$api = Mage::getModel ("cloudflare/api_request");
+			$api->setType ( $this->_usePatchToSet ? $api::REQUEST_PATCH : $api::REQUEST_PUT );
+			$api->setData ( array (
+				"description" => $desc,
+				"urls" => $urls,
+				"configurations" => $config,
+				"paused" => $paused,
+				"priority" => $priority
+			));
+			return $api->resolve ( $endpoint );
+		}
+
+		/**
+		 * This method takes in a zone lockdown rule id and it attempts to
+		 * delete it using the Cloudflare API.
+		 * @param   string       id                   Page rule ID
+		 * @return  stdClass                          CF response to request
+		 */
+		public function delete ( $id ) {
+			$endpoint = $this->getEndpoint ( $this->_endpoint . "/$id" );
+			$api = Mage::getModel ("cloudflare/api_request");
+			$api->setType ( $api::REQUEST_DELETE );
+			return $api->resolve ( $endpoint );
+		}
+
+	}
---
+++ src/app/code/community/JetRails/Cloudflare/controllers/Cloudflare/Api/Firewall/ZoneLockdownController.php
@@ -0,0 +1,91 @@
+<?php
+
+	/**
+	 * This controller inherits from a generic controller that implements the
+	 * base functionality for interfacing with a getter model. This action
+	 * simply loads the initial value through the Cloudflare API. The rest of
+	 * this class extends on that functionality and adds more endpoints.
+	 * @version     1.1.7
+	 * @package     JetRails® Cloudflare
+	 * @author      Rafael Grigorian <development@jetrails.com>
+	 * @copyright   © 2018 JETRAILS, All rights reserved
+	 * @license     MIT https://opensource.org/licenses/MIT
+	 */
+	class JetRails_Cloudflare_Cloudflare_Api_Firewall_ZoneLockdownController
+	extends JetRails_Cloudflare_Controller_Getter {
+
+		/**
+		 * This method takes in all the information that is necessary for
+		 * creating a zone lockdown rule through the request parameters. It then
+		 * asks the Cloudflare API model to create said zone lockdown rule.
+		 * @return  void
+		 */
+		public function createAction () {
+			$api = Mage::getModel ("cloudflare/api_firewall_zoneLockdown");
+			$response = $api->create (
+				$this->_request->getParam ("description"),
+				$this->_request->getParam ("urls"),
+				$this->_request->getParam ("configurations"),
+				$this->_request->getParam ("paused") == "true",
+				$this->_request->getParam ("priority") ? intval ( $this->_request->getParam ("priority") ) : null
+			);
+			return $this->_sendResponse ( $response );
+		}
+
+		/**
+		 * This action takes in all the information that is necessary for
+		 * editing a zone lockdown rule though the request parameters. It then
+		 * asks the Cloudflare API model to update the values of the zone
+		 * lockdown rule with the corresponding zone lockdown rule id.
+		 * @return  void
+		 */
+		public function editAction () {
+			$api = Mage::getModel ("cloudflare/api_firewall_zoneLockdown");
+			$response = $api->edit (
+				$this->_request->getParam ("id"),
+				$this->_request->getParam ("description"),
+				$this->_request->getParam ("urls"),
+				$this->_request->getParam ("configurations"),
+				$this->_request->getParam ("paused") == "true",
+				$this->_request->getParam ("priority")
+					? intval ( $this->_request->getParam ("priority") )
+					: null
+			);
+			return $this->_sendResponse ( $response );
+		}
+
+		/**
+		 * This action simply takes in a zone lockdown rule id and a state value
+		 * though the request parameters. It then asks the Cloudflare API to
+		 * update the state of the zone lockdown rule based on the passed
+		 * corresponding id.
+		 * @return  void
+		 */
+		public function toggleAction () {
+			$api = Mage::getModel ("cloudflare/api_firewall_zoneLockdown");
+			$response = $api->edit (
+				$this->_request->getParam ("id"),
+				$this->_request->getParam ("description"),
+				$this->_request->getParam ("urls"),
+				$this->_request->getParam ("configurations"),
+				$this->_request->getParam ("paused") == "true",
+				$this->_request->getParam ("priority")
+					? intval ( $this->_request->getParam ("priority") )
+					: null
+			);
+			return $this->_sendResponse ( $response );
+		}
+
+		/**
+		 * This action takes in a zone lockdown rule id through the request
+		 * parameters. It then asks the Cloudflare API model to delete the zone
+		 * lockdown rule with the corresponding zone lockdown rule id.
+		 * @return  void
+		 */
+		public function deleteAction () {
+			$api = Mage::getModel ("cloudflare/api_firewall_zoneLockdown");
+			$response = $api->delete ( $this->_request->getParam ("id") );
+			return $this->_sendResponse ( $response );
+		}
+
+	}
